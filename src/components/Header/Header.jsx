53f74b23e282e0514e3c23c3498a6d22a019a882
Anant <p.anant223@gmail.com>
2024-11-20 20:27:44 +0530

    designs header with tailwind css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
-import {Logo} from "../index"
+import {Container, Logo} from "../index"
 
 const Header = () => {
   return (
-    <div>
-        <Logo/>
-    </div>
-  )
+    <header className=" bg-yellow-950 py-4">
+      <Container>
+        <nav className="flex justify-between w-full items-center">
+          <div>
+            <Logo />
+          </div>
+          <div>
+            <ul className="flex w-full space-x-8 text-lg font-medium  transition-colors text-white items-center">
+              <li className="hover:text-yellow-300">
+                <a href="#">Features</a>
+              </li>
+              <li className="hover:text-yellow-300">
+                <a href="#">How it Works</a>
+              </li>
+              <li className="hover:text-yellow-300">
+                <a href="#">Contact</a>
+              </li>
+              <li className="hover:text-yellow-300">
+                <a href="#">Login</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </Container>
+    </header>
+  );
 }
 
 export default Header
