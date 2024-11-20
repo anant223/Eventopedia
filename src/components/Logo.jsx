5bc8f6ba2515fdfca63dc22f024f98eeb8e21947
Anant <p.anant223@gmail.com>
2024-11-20 17:40:32 +0530

    created componets for home page

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-pink-500">Eventopida</h1>
+    </div>
+  );
+}
+
+export default Logo
