3d74b4c13597aad69efd6125c7c1d23accf65eba
Anant <p.anant223@gmail.com>
2024-11-20 20:51:25 +0530

    header fixing error

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Container = ({ children}) => {
   return (
     <div
-      className={`max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8`}
+      className={` container mx-auto px-4 sm:px-6 lg:px-8`}
     >
       {children}
     </div>
