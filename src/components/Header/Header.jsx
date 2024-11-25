9732a143f77bbebefd035b11ffe820687bb5272e
Anant <p.anant223@gmail.com>
2024-11-25 11:08:05 +0530

    improve header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
           <div className="text-2xl font-bold text-yellow-500 w-full sm:w-auto">
             <Logo />
           </div>
-          <ul className="hidden md:flex space-x-6 relative text-base mx-auto font-bold">
+          <ul className="hidden md:flex space-x-6 relative text-[16px] right-12 font-bold ">
             <li>
               <a href="#how-it-works" className="hover:text-yellow-500">
                 Features
