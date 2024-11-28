d3ddb0721d3e31a55dee426d9b3445b7c50ee7af
Anant <p.anant223@gmail.com>
2024-11-28 17:44:54 +0530

    debugging home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import { Container } from "../components/index.js";
 
 const Home = () => {
   return (
-    <div className="bg-gray-950 max-w-7xl px-2 sm:px-4 lg:px-6">
+    <div className="bg-gray-950">
       <HeroSection />
       <Features />
       <HowItWork />
