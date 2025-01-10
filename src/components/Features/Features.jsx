00aea9560f1cbbeab8585b208dd34de25a9fc62b
Anant <p.anant223@gmail.com>
2025-01-10 23:48:27 +0530

    fix the landing page bug

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -9,15 +9,14 @@ import { Carousel } from "antd";
 const Features = () => {
   return (
     <section className=" bg-[#F5F5F7] text-gray font-roboto">
-      <div className="">
-        <div className=" w-full flex justify-center">
+      <div>
+        <div className=" w-full pl-4 sm:pl-0">
           <h3 className="text-2xl lg:text-4xl lg:text-center font-bold text-gray-600">
             Powerful Features
           </h3>
         </div>
-
         <div className="relative w-full group pl-4 sm:pl-44">
-          <div className="absolute left-0 top-1/2 -translate-y-1/2 z-10 hover:bg-gray-200 rounded-full p-2  opacity-0 group-hover:opacity-100 transition-opacity duration-300 hover:duration-300 hover:transition ">
+          <div className="absolute left-0 top-1/2 -translate-y-1/2 z-10 hover:bg-gray-200 rounded-full p-2  opacity-0 group-hover:opacity-100 transition-opacity duration-300 hover:duration-300 hover:transition sm:pl-44 ">
             <LeftCircleOutlined className="text-5xl cursor-pointer hover:opacity-75 transition-opacity" />
           </div>
 
