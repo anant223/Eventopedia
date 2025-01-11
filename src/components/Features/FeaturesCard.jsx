c8f6a280a463e6cf9c6dbba10ff0bf7bdde136ee
Anant <p.anant223@gmail.com>
2025-01-11 09:33:51 +0530

    fix the landing page bug

--- src/components/Features/FeaturesCard.jsx
+++ src/components/Features/FeaturesCard.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const FeaturesCard = ({icon, title, desc, bgColor}) => {
   return (
-    <div className=" bg-white lg:w-[440px] w-[340px] text-black min-h-[520px]  rounded-3xl  ">
+    <div className=" bg-white lg:w-390px] w-[340px] text-black min-h-[520px]  rounded-3xl  ">
       <div className="p-8">
         <div className="w-16 h-16 mb-8 rounded-full flex  justify-center bg-gradient-to-br items-center bg-gray-800 text-gray-200">
           {icon}
