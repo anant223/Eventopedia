ddb6764680fb176a0bf8b4dd804584a397fb6add
Anant <p.anant223@gmail.com>
2025-01-28 17:51:59 +0530

    added text size

--- src/components/EventSidebar/EventSidebar.jsx
+++ src/components/EventSidebar/EventSidebar.jsx
@@ -29,7 +29,7 @@ const EventSidebar = () => {
                   <span>
                     <item.icon className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
                   </span>
-                  <span className="ms-3">{item.label}</span>
+                  <span className="ms-3 text-xl">{item.label}</span>
                 </button>
               </li>
             ))}
