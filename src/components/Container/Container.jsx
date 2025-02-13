adb62c5920f6c4e6bac2164effbc498f1b7e179c
Anant <p.anant223@gmail.com>
2025-02-13 13:06:36 +0530

    updating landing page

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Container = ({ children}) => {
   return (
-    <div className=" max-w-[1500px] mx-auto px-4 md:px-8 lg:px-12">{children}</div>
+    <div className=" max-w-[1400px] mx-auto px-4 md:px-8 lg:px-12">{children}</div>
   );
 };
 
