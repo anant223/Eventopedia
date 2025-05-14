d3ff8dce240a07061c455cd4636631c40775f443
Anant <p.anant223@gmail.com>
2025-05-14 12:53:16 +0530

    .

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -7,9 +7,9 @@ import {Button} from "../index.js"
 const HeroSection = () => {
   const navigate = useNavigate();
   return (
-    <section className="dark:text-text relative overflow-hidden">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 w-full lg:pb-56 md:px-10 lg:px-32 lg:pt-48">
-        <div className="max-w-3xl mx-auto text-center">
+    <section className="dark:text-text relative overflow-hidden h-dvh flex items-center justify-center">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 w-full md:px-10 lg:px-32">
+        <div className="flex flex-col items-center justify-center text-center">
           <motion.h1
             initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
@@ -28,14 +28,18 @@ const HeroSection = () => {
             initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.4 }}
-            className="mb-8 text-lg sm:mb-8 dark:text-text"
+            className="mb-8 text-lg sm:mb-8 dark:text-text max-w-2xl"
           >
-           GRUPIO lets you host and stream virtual events — public or private — and connect with professionals in real time, right from your browser.
+            GRUPIO lets you host and stream virtual events — public or private —
+            and connect with professionals in real time, right from your
+            browser.
           </motion.p>
+
           <motion.div
             initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.6 }}
+            className="mb-8"
           >
             <Button
               name="Register"
