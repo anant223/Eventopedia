f2f366efa034bee44c410e608947867492ae4789
Anant <p.anant223@gmail.com>
2025-07-12 12:16:00 +0530

    improve hero section

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -41,7 +41,7 @@ const HeroSection = () => {
       </div>
       <div className="mx-auto relative max-w-4xl px-4 sm:px-6 text-center lg:px-8">
         <div className=" relative">
-          <motion.div className=" mb-6 sm:mb-8 font-bricolage font-medium m-[0_auto]  px-2 py-1 rounded-full border-gray-700 border bg-gray-500/10 text-text w-fit mt-20 transform-none opacity-100 flex items-center text-xs md:text-base">
+          <motion.div className=" mb-6 sm:mb-8 font-bricolage font-medium m-[0_auto]  px-2 py-1 rounded-full border-gray-700 border bg-gray-500/10 text-text w-fit mt-20 transform-none opacity-100 flex items-center text-xs md:text-base max-w-fit overflow-hidden h-fit">
             <div className="w-2 h-2 bg-blue-400 rounded-full mr-2 animate-pulse "></div>
             Now In Beta - Join Early Access
           </motion.div>
