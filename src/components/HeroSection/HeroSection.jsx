6959ed05eeac90b3fb7536e54665b37fbe8c7810
Anant <p.anant223@gmail.com>
2025-07-12 22:20:40 +0530

    improve hero section

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -35,36 +35,33 @@ const HeroSection = () => {
 
   
   return (
-    <section className="relative overflow-hidden  bg-background min-h-screen flex justify-center sm:text-center sm:px-18 py-16 sm:py-20">
-      <div>
+    <section className="relative overflow-hidden  bg-background min-h-screen flex justify-center sm:text-center py-16 sm:py-20">
+      <div className=" absolute inset-0">
         <FloatingParticle />
         <div className="absolute inset-0 bg-[linear-gradient(rgba(255,255,255,0.02)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.02)_1px,transparent_1px)] bg-[size:50px_50px]" />
       </div>
       <div className="mx-auto relative container px-4 sm:px-6 lg:px-8">
-        <div className="relative">
-          <motion.div className=" font-bricolage mb-4 sm:mb-6 font-medium sm:m-[0_auto]  px-2 py-1 rounded-full border-gray-700 border bg-gray-500/10 text-text w-fit mt-20 transform-none opacity-100 flex items-center text-xs md:text-base max-w-fit overflow-hidden h-fit">
-            <div className="w-2 h-2 bg-blue-400 rounded-full mr-2 animate-pulse "></div>
+        <div className="relative max-w-4xl mx-auto">
+          <motion.div className="font-bricolage mb-6 sm:mb-8 font-medium mx-auto px-3 py-2 rounded-full border border-gray-700 bg-gray-500/10 text-text w-fit flex items-center h-fit text-xs md:text-sm mt-8 sm:mt-12">
+            <div className="w-2 h-2 bg-blue-400 rounded-full mr-2 animate-pulse" />
             Now In Beta - Join Early Access
           </motion.div>
-          <motion.h1 className="font-bold mb-8 text-4xl text-[3rem] sm:text-4xl md:text-5xl lg:text-6xl font-bricolage text-text">
-            <span className="inline-block">The </span>{" "}
-            <span className="inline-block">ultimate </span>{" "}
-            <span className="inline-block">platform to </span>
+          <motion.h1 className="font-bold mb-6 font-bricolage text-text text-6xl sm:text-5xl md:text-6xl lg:text-7xl ">
+            <span className="inline-block">The ultimate platform to </span>
             <br className="hidden sm:block" />
-            <div className=" inline-block sm:bg-[#2e1a86] rounded-lg p-2 text-5xl lg:6xl sm:text-none shadow-2xl">
+            <div className=" inline-block mt-2 sm:bg-[#2e1a86] rounded-lg p-2 text-5xl lg:6xl sm:text-none shadow-2xl">
               <span className="bg-gradient-to-tr from-[#2e1a86] via-[#8bb4e9] to-[#924cdd] bg-clip-text text-transparent font-CS inline-block">
                 Host/Attend
               </span>
             </div>{" "}
-            <span className="inline-block">live </span>{" "}
-            <span className="inline-block">events.</span>{" "}
+            <span className=" inline-block">live events. </span>
           </motion.h1>
-          <motion.div className="mb-8 font-bricolage text-xl  md:text-2xl text-text mx-auto max-w-2xl">
+          <motion.p className="mb-8 font-bricolage text-lg sm:text-xl md:text-2xl text-text mx-auto max-w-3xl">
             Host and attend virtual or in-person events — public or private —
             and connect with professionals in real time, right from your
             browser.
-          </motion.div>
-          <motion.div className="mb-6 sm:mb-8 scale-100 hover:scale-105 transition-transform duration-300">
+          </motion.p>
+          <motion.div className=" transform hover:scale-105 transition-transform duration-300">
             <Button
               name="Let's create"
               handleClick={() => navigate("/auth?type=signup")}
