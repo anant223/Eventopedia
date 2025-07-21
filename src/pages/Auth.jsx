9e2e6135615182e2b7a8f5458ab09a5f08605c09
Anant <p.anant223@gmail.com>
2025-07-21 22:54:24 +0530

    improve auth and auth-page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
-import { Login, Signup } from "../components/auth";
+import {Signup, Login} from "../components/auth/index.js"
 
 const Auth = () => {
   const [searchParam, setSearchParam] = useSearchParams();
