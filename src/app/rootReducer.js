4fb023a863c87f7b915eed42df9ecc46cb78a1c3
Anant <p.anant223@gmail.com>
2026-06-13 13:40:39 +0530

    feat: redesign landing page hero section

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -3,10 +3,10 @@ import authReducer from "@/app/slices/authSlice.js";
 import eventsReducer from "@/app/slices/eventsSlice.js";
 import likesReducer from "@/app/slices/likeSlice.js";
 import registrationReducer from "@/app/slices/registerSlice";
-import bookingReducer from "@/app/slices/registerSlice";
-import paymentReducer from "@/app/slices/paymentSlice";
-import notificationReducer  from "@/app/slices/notificationSlice";
-import categoryReducer from "@/app/slices/categorySlice"
+// import bookingReducer from "@/app/slices/registerSlice";
+// import paymentReducer from "@/app/slices/paymentSlice";
+// import notificationReducer  from "@/app/slices/notificationSlice";
+// import categoryReducer from "@/app/slices/categorySlice"
 import waitlistReducer from "@/app/slices/waitlistSlice"
 
 const rootReducer = combineReducers({
@@ -14,10 +14,10 @@ const rootReducer = combineReducers({
   event: eventsReducer,
   likes: likesReducer,
   registration: registrationReducer,
-  booking: bookingReducer,
-  payment: paymentReducer,
-  notifications: notificationReducer,
-  category: categoryReducer,
+  // booking: bookingReducer,
+  // payment: paymentReducer,
+  // notifications: notificationReducer,
+  // category: categoryReducer,
   waitlist: waitlistReducer
 });
 
