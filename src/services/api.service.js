00cdf4a2fcd388992ff3e20bd05569ea58406f19
Anant <p.anant223@gmail.com>
2026-06-13 16:10:05 +0530

    fixed the bug of api service

---
+++ src/services/api.service.js
@@ -0,0 +1,41 @@
+import store from "@/app/store";
+import AppService from "./app.service";
+import { clearUser } from "@/app/slices/authSlice";
+
+export default class ApiService extends AppService {
+  constructor() {
+    super();
+    this.baseURL = `${this.baseURI}/api/v1`;
+    this.axiosInstance = this.getAxiosInstance({
+      baseURL: this.baseURL,
+    });
+    this.setupInterceptors()
+  }
+  setupInterceptors() {
+    this.axiosInstance.interceptors.response.use(
+      (res) => res,
+      (err) => {
+      const status = err.response?.status;
+      const url = err.config?.url;
+
+      if (status === 401) {
+        const isAuthRoute =
+            url?.includes("/login") || url?.includes("/register");
+
+        if (isAuthRoute) {
+          return Promise.reject(err);
+        }
+
+        // store.dispatch(clearUser());
+        console.log("it cleared")
+
+        // if (!window.location.pathname.includes('/auth')) {
+        //   window.location.href = "/auth?type=login";
+        // }
+        return Promise.reject(err)
+      }
+      return Promise.reject(err);
+      }
+    );
+  }
+}
