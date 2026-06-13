9620597aa34b9a5fc74d730ab581c82fbd396a9a
Anant <p.anant223@gmail.com>
2026-06-13 16:05:45 +0530

    fixed the rootReducer issue

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,24 +1,16 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import authReducer from "@/app/slices/authSlice.js";
-import eventsReducer from "@/app/slices/eventsSlice.js";
-import likesReducer from "@/app/slices/likeSlice.js";
-import registrationReducer from "@/app/slices/registerSlice";
-// import bookingReducer from "@/app/slices/registerSlice";
-// import paymentReducer from "@/app/slices/paymentSlice";
-// import notificationReducer  from "@/app/slices/notificationSlice";
-// import categoryReducer from "@/app/slices/categorySlice"
-import waitlistReducer from "@/app/slices/waitlistSlice"
+// import authReducer from "@/app/slices/authSlice.js";
+// import eventsReducer from "@/app/slices/eventsSlice.js";
+// import likesReducer from "@/app/slices/likeSlice.js";
+// import registrationReducer from "@/app/slices/registerSlice";
+import waitlistReducer from "@/app/slices/waitlistSlice";
 
 const rootReducer = combineReducers({
-  auth: authReducer,
-  event: eventsReducer,
-  likes: likesReducer,
-  registration: registrationReducer,
-  // booking: bookingReducer,
-  // payment: paymentReducer,
-  // notifications: notificationReducer,
-  // category: categoryReducer,
-  waitlist: waitlistReducer
+  // auth: authReducer,
+  // event: eventsReducer,
+  // likes: likesReducer,
+  // registration: registrationReducer,
+  waitlist: waitlistReducer,
 });
 
 export default rootReducer;
