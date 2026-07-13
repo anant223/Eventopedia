0fc7710254fee33771048253b1b779c358658b83
Anant <p.anant223@gmail.com>
2026-07-14 00:54:16 +0530

    Fix the import issue

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Outlet, useNavigate, useLocation } from "react-router-dom";
-import { AuthenticatedHeader } from "@/components/headers/AuthenticatedHeader";
+import AuthenticatedHeader from "@/components/headers/AuthenticatedHeader";
 
 
 
