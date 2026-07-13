68046c962223ced1d93768b68259ee499f9d7733
Anant <p.anant223@gmail.com>
2026-07-14 00:26:49 +0530

    op the vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
+import svgr from "vite-plugin-svgr";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api/v1": "http://localhost:4000",
-    },
-  },
-  plugins: [react()],
+  plugins: [react(), svgr()],
   base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api/v1": "http://localhost:4000",
     },
   },
 });
