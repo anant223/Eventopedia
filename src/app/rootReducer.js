1871a4e3fd6b6186611790585f021114629c389d
Anant <p.anant223@gmail.com>
2026-07-13 17:45:16 +0530

    added more auth,events, and others

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,28 +1,27 @@
 import { combineReducers } from "@reduxjs/toolkit";
-// import authReducer from "@/app/slices/authSlice.js";
-// import eventsReducer from "@/app/slices/eventsSlice.js";
+import authReducer from "@/app/slices/authSlice.js";
+import eventsReducer from "@/app/slices/eventsSlice.js";
 // import likesReducer from "@/app/slices/likeSlice.js";
 // import registrationReducer from "@/app/slices/registerSlice";
+// import notificationReducer from "@/app/slices/notificationSlice";
+// import categoryReducer from "@/app/slices/categorySlice";
+// import paymentReducer from "@/app/slices/paymentSlice";
 import waitlistReducer from "@/app/slices/waitlistSlice";
+import otherReducer from "@/app/slices/otherSlice";
 
-// const rootReducer = combineReducers({
-  // auth: authReducer,
-  // event: eventsReducer,
-  // likes: likesReducer,
-  // registration: registrationReducer,
-//   waitlist: waitlistReducer,
-// });
 
 const rootReducer = combineReducers({
-  auth: (
-    state = {
-      isAuthenticated: false,
-      user: null,
-      initialized: false,
-      loading: false,
-    }
-  ) => state,
+  auth: authReducer,
+  event: eventsReducer,
+  likes: likesReducer,
+  registration: registrationReducer,
+  notifications: notificationReducer,
+  category: categoryReducer,
+  payment: paymentReducer,
   waitlist: waitlistReducer,
+  others: otherReducer
 });
+
+
 
 export default rootReducer;
