393aa95237d61f9aa898694b0d6aba7c21d2262c
Anant <p.anant223@gmail.com>
2026-07-13 23:59:30 +0530

    Add in services index.js

---
+++ src/services/index.js
@@ -0,0 +1,24 @@
+// import LikeService from "./like.service";
+// import RegisterService from "./register.service";
+import UserService from "./user.service";
+// import PaymentService from "./payment.service";
+import EventService from "./event.service"
+import ApiService from "./api.service";
+import  OtherService  from "./other.service";
+import LocationService from "./location.service";
+// import BookingService from "./booking.service";
+// import CategoryService from "./category.service";
+
+
+export {
+    // LikeService,
+    // RegisterService,
+    UserService,
+    // PaymentService,
+    EventService,
+    ApiService,
+    OtherService,
+    LocationService,
+    // BookingService,
+    // CategoryService
+}
