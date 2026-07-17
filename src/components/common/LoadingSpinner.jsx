f20323cd5dec7b683335663667f2952e9f2101e1
Anant <p.anant223@gmail.com>
2026-07-18 01:09:43 +0530

    add new spinner

--- src/components/common/LoadingSpinner.jsx
+++ src/components/common/LoadingSpinner.jsx
@@ -1,7 +1,7 @@
 export default function CenteredSpinner() {
   return (
     <div
-      className="flex items-center justify-center absolute inset-0 z-40 bg-background"
+      className="flex items-center justify-center absolute inset-0 z-40 bg-[#f0ede6]"
       aria-disabled
     >
       <style>{`
