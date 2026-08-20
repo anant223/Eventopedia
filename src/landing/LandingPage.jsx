872d8d6d56d93ba058d663dc3a541ce13313cad5
Anant <p.anant223@gmail.com>
2026-08-20 09:06:16 +0530

    modified

--- src/landing/LandingPage.jsx
+++ src/landing/LandingPage.jsx
@@ -7,11 +7,11 @@ const LandingPage = ({
   founderName   = "Anant",
   founderAvatar = null,
 }) => (
-  <div className="min-h-screen bg-[#f0ede6] font-roboto">
+  <>
     <HeroSection onJoinWaitlist={onJoinWaitlist} />
     <FeaturesSection />
     <StorySection name={founderName} avatar={founderAvatar} />
-  </div>
+  </>
 );
 
 export default LandingPage;
