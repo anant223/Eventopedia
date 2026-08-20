3e9259563b25d0e0fe029f86b94515e42f358700
Anant <p.anant223@gmail.com>
2026-08-20 09:04:54 +0530

    redsgin event read form

--- src/pages/EventDetail.jsx
+++ src/pages/EventDetail.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import EventReadPage from "@/event-detail/eventPage"
-import { PageShell } from '@/components/layout/PageShell';
+import { useParams } from 'react-router-dom';
 const Event = () => {
-
+  const {id} = useParams()
   return (
-    <PageShell>
-      <EventReadPage/>
-    </PageShell>
+    <div className='w-full h-full overflow-y-auto'>
+      <EventReadPage eventId={id} />
+    </div>
   );
-}
+};
 
 export default Event
